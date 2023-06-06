87329c49e3fe1a83440e935a5b5612d26ad73ee5
neokkk <seven3126@gmail.com>
2023-06-06 22:21:36 +0900

    feat: 상품 도메인

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,8 +2,8 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype") // 상속 관계 매핑을 위한 구분 컬럼 설정
-@Getter @Setter
+@Getter
 public abstract class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_id")
@@ -22,4 +22,14 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) throw new NotEnoughStockException("need more stock");
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+  @PersistenceContext
+  private final EntityManager entityManager;
+
+  public void save(Item item) {
+    if (item.getId() == null) {
+      entityManager.persist(item);
+    } else {
+      entityManager.merge(item);
+    }
+  }
+
+  public Item findOne(Long id) {
+    return entityManager.find(Item.class, id);
+  }
+
+  public List<Item> findAll() {
+    return entityManager.createQuery("select i from Item i", Item.class)
+            .getResultList();
+  }
+}
